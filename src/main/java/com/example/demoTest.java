1909e81034ec3823bd6f4902c418dcc5edc09653
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2025-08-19 15:19:36 +0800

    rename test and main files, updated pom.xml file for main configuration

--- src/main/java/com/example/demoTest.java
+++ src/main/java/com/example/demoTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GoogleTest {
+public class demoTest {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
 
--- src/test/java/com/example/test.java
+++ src/test/java/com/example/test.java
@@ -11,7 +11,7 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class FirstTest {
+public class test {
 
     WebDriver driver;
 

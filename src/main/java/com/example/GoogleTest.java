8d6113ec6cee9699c07cd0e0912901c7abc104b4
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2025-08-19 12:32:32 +0800

    Initial commit: add Maven Selenium Test project

---
+++ src/main/java/com/example/GoogleTest.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleTest {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        // Open Google
+        driver.get("https://www.google.com");
+        Thread.sleep(2000); // tunggu 2s
+
+        // Finding text box
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium WebDriver");
+        Thread.sleep(2000); // tunggu 2s
+
+        // Submit search
+        searchBox.submit();
+        Thread.sleep(3000); // tunggu 3s nak tengok result
+
+        // Getting title page
+        System.out.println("Page title: " + driver.getTitle());
+
+        driver.quit();
+    }
+}
---
+++ src/test/java/com/example/FirstTest.java
@@ -0,0 +1,48 @@
+package com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testFormSubmission() {
+        
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+
+        Assert.assertEquals(value, "Received!");
+    }
+
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
